f6b74287b12548fcb05fde69e5038c2bd0655853
Alexandre C. <alexandre.corroy@gmail.com>
2025-06-23 14:45:48 +0200

    update v2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const dotenv = require("dotenv")
 dotenv.config()
 const app = express()
+const fs = require("fs")
 
 const PORT = process.env.PORT || 3000
 
@@ -10,6 +11,23 @@ app.use(express.json())
 app.get("/", (req, res) => {
     console.log("GET on /...")
     res.send("Hello world!")
+})
+
+app.get("/comments", (req, res) => {
+    fs.readFile("comments.txt", (err, data) => {
+        if (err) throw err
+        res.send(data.toString())
+    })
+})
+
+app.post("/comments", (req, res) => {
+    const { body } = req
+    const message = body.message
+    fs.appendFile("comments.txt", `${message}\n`, err => {
+        if (err) throw err;
+        res.send('Added to file')
+    }); 
+
 })
 
 app.listen(PORT, () => {
